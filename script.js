03ef64fb6d0511836741b5b3121ed34bf271582e
Dave <davidgeorgebell1995@outlook.com>
2022-10-23 13:46:59 +0100

    a bit of function

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-let buttonColors = ["red", "blue", "green", "yellow"];
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
 
-//random number generator between 0-3..
-function buttonSeqRandomizer() {
+//start game keypress..
+$(document).one("keydown",function() {
+  $('h1').text("level " + level);
+  nextSequence();
+});
+
+//gameSeq..
+function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+  $('#' + randomChosenColor).fadeOut(100).fadeIn(100);
+  let audio = new Audio('sounds/' + randomChosenColor + '.mp3');
+  audio.play();
+  console.log(gamePattern);
+}
+
+//User click event..
+$(".btn").click(function () {
+  userClickedPattern.push(this.id);
+  animateClick(this.id)
+  checkAnswer(this.id)
+  console.log(userClickedPattern);
+});
+
+//click animation and audio..
+function animateClick(currentColor) {
+  let audio = new Audio("sounds/" + currentColor + ".mp3");
+  audio.play();
+$("#" + currentColor).addClass("pressed");
+setTimeout(function () {
+  $("#" + currentColor).removeClass("pressed");
+}, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if (currentLevel == gamePattern) {
+      userClickedPattern.length = 0;
+      level++
+      $('h1').text("level " + level);
+      console.log('correct!')
+      setTimeout(() => {
+        nextSequence()
+      }, 1000);
+  } else {
+    $('h1').text("Game Over!");
+    animateClick = null;
+  }
 }
